9dd0ad535d4177cd1cbf3dadbbf353520bced3c0
Rasmus Schlünsen <rasmusrasmus@schlunsen.com>
2015-04-27 23:49:49 +0200

    initial commit

---
+++ client.js
@@ -0,0 +1,27 @@
+var tls = require('tls'),
+    fs = require('fs');
+
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+var options = {
+  key: fs.readFileSync('private-key.pem'),
+  cert: fs.readFileSync('public-cert.pem'),
+  rejectUnhauthorized : false
+};
+
+var conn = tls.connect(8000, options, function() {
+  if (conn.authorized) {
+    console.log("Connection authorized by a Certificate Authority.");
+  } else {
+    console.log("Connection not authorized: " + conn.authorizationError)
+  }
+    console.log();
+});
+
+
+
+conn.on("data", function (data) {
+  console.log(data.toString());
+  conn.end();
+});
